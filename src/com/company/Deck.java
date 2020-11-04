26f7df8a99a77a4417f58e3feccb35bd6074af3e
unknown <marcnadal7@gmail.com>
2020-11-03 22:31:23 -0600

    Pequeños cambios y formato

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -43,31 +44,47 @@ public class Deck{
 
     public String Suffle(){
         if(list1.isEmpty()){
-            return "El deck esta vaciío";
+            try {
+                JOptionPane.showMessageDialog(null, "El deck esta vaciío");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error");
+            }
         }else{
             System.out.println("\nShuffle");
             Collections.shuffle(list1);
             System.out.println("El deck a sido mezclado");
             return "El deck a sido mezclado";
         }
+        return "El deck a sido mezclado";
     }
 
     public String Head(){
+        Card c = null;
         if(list1.isEmpty()){
-            return "El deck esta vaciío";
+            try {
+                JOptionPane.showMessageDialog(null, "El deck esta vaciío");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error");
+            }
         }else {
             System.out.println("\nHead");
-            Card c = list1.get(1);
+            c = list1.get(1);
             System.out.println(c);
             list1.remove(1);
             System.out.println(String.format("Quedan %s cartas en el Deck", list1.toArray().length));
             return String.format("%s \n Quedan %s cartas en el Deck", c, list1.toArray().length);
         }
+        return String.format("%s \n Quedan %s cartas en el Deck", c, list1.toArray().length);
     }
 
     public String Pick(){
+        Card c = null;
         if(list1.isEmpty()){
-            return "El deck esta vaciío";
+            try {
+                JOptionPane.showMessageDialog(null, "El deck esta vaciío");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error");
+            }
         }else {
             System.out.println("\nPick");
             int aleatorio = (int) Math.floor(Math.random() * (0 - 53 + 1) + 53);
@@ -75,21 +92,27 @@ public class Deck{
             if (list1.toArray().length < aleatorio) {
                 aleatorio = list1.toArray().length;
             }
-            Card c = list1.get(aleatorio);
+            c = list1.get(aleatorio);
             System.out.println(c);
             list1.remove(aleatorio);
             System.out.println("Quedan" + list1.toArray().length + " cartas en el Deck");
             String.format("Quedan %s cartas en el Deck", list1.toArray().length);
             return String.format("%s \n Quedan %s cartas en el Deck", c, list1.toArray().length);
         }
+        return String.format("%s \n Quedan %s cartas en el Deck", c, list1.toArray().length);
     }
 
     public String Hand(){
+        String msg = "";
         if(list1.isEmpty()){
-            return "El deck esta vaciío";
+            try {
+                JOptionPane.showMessageDialog(null, "El deck esta vaciío");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error");
+            }
         }else {
             System.out.println("\nHand");
-            String msg = "";
+
             for (int x = 0; x < 5; x++) {
                 Card c = list1.get(x);
                 msg += c.toString() + "\n";
@@ -99,5 +122,6 @@ public class Deck{
             System.out.println(String.format("%s Quedan %s", msg, list1.toArray().length));
             return String.format("%s Quedan %s cartas en el Deck", msg, list1.toArray().length);
         }
+        return String.format("%s Quedan %s cartas en el Deck", msg, list1.toArray().length);
     }
 }
